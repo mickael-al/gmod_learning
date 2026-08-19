8b37ee85e9a549c2410475cb857bb6f279872a19
U-DESKTOP-7B6PDR8\m <mickael.albarello77@gmail.com>
2026-08-19 13:37:15 +0200

    gmod learning

---
+++ TestDll/TestDll.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved)
+{
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) 
+    {
+        MessageBoxA(NULL, "DLL injectée avec succès !", "TEST DLL", MB_OK);
+    }
+    return TRUE;
+}
